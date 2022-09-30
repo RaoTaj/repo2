98a969336204dae0c1aa4a453d574c15672531f5
RaoTaj <mrsikandar6843@gmail.com>
2022-09-30 20:29:20 +0500

    repo

---
+++ public/app.js
@@ -0,0 +1,14 @@
+function circle(){
+    var rao = document.getElementById("one");
+rao.className = "square";
+}
+
+function square(){
+    var rao = document.getElementById("two");
+    rao.className = "rectangle";
+}
+
+function rectangle(){
+    var rao  = document.getElementById("three");
+rao.className = "circle";
+}
